88070f347b67dcc0af0104961c910ba653761064
Pekka Turtola <pekka.turtola@optima.fi>
2020-04-04 20:57:50 +0300

    Add react infinite scroller

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import store from './store';
 import ImageListView from './views/ImageListView';
 import ImageView from './views/ImageView';
+import { CssBaseline } from '@material-ui/core';
 
 function App() {
   return (
     <Provider store={store}>
+      <CssBaseline />
       <BrowserRouter>
         <div className="App">
           <Switch>
--- src/views/ImageListView.tsx
+++ src/views/ImageListView.tsx
@@ -1,11 +1,18 @@
-import { Button } from '@material-ui/core';
-import React, { useEffect } from 'react';
+import { LinearProgress, withStyles } from '@material-ui/core';
+import React from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
 import { useDispatch, useSelector } from 'react-redux';
 import ImageList from '../components/ImageList';
+import useWindowSize from '../hooks/useWindowSize';
 import { fetchPhotos } from '../store/actions';
 import { IAppState } from '../store/reducer';
-import LoadingList from '../components/LoadingList';
-import useWindowSize from '../hooks/useWindowSize';
+
+const StyledProgress = withStyles({
+    root: {
+        height: 20,
+        margin: 5,
+    },
+})(LinearProgress)
 
 const ImageListView = () => {
     const dispatch = useDispatch();
@@ -14,22 +21,26 @@ const ImageListView = () => {
 
     const tileSize = 150;
     const columnCount = Math.floor(windowSize.width / tileSize) || 1
-    const spacing = columnCount > 1 ? Math.floor((windowSize.width % tileSize) / (columnCount - 1)) : 0 
+    const spacing = columnCount > 1 ? Math.floor((windowSize.width % tileSize) / (columnCount - 1)) : 0
     const rowCount = Math.floor(windowSize.height / tileSize) || 1
     const fetchSize = Math.max(20, columnCount * rowCount * 2);
+    const loadMore = () => dispatch(fetchPhotos(fetchSize));
 
-    useEffect(() => {
-        if (photos.length === 0) {
-            dispatch(fetchPhotos(fetchSize));
-        }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    return photos.length === 0 ? <LoadingList columnCount={columnCount} spacing={spacing} tilesCount={fetchSize} tileSize={tileSize} /> : (
-        <>
-            <ImageList photos={photos} columnCount={columnCount} spacing={spacing} tileSize={tileSize} />
-            <Button onClick={() => dispatch(fetchPhotos(fetchSize))}>Lataa lisää</Button>
-        </>
+    return (
+        <InfiniteScroll
+            pageStart={1}
+            loadMore={loadMore}
+            hasMore={photos.length < 5000}
+            loader={<StyledProgress key="loading" />}
+        >
+            <ImageList
+                key="images"
+                photos={photos}
+                columnCount={columnCount}
+                spacing={spacing}
+                tileSize={tileSize}
+            />
+        </InfiniteScroll>
     )
 }
 
