4e02137dade7a1c679753db8cd5e36623cb7c852
Pekka Turtola <pekka.turtola@optima.fi>
2020-04-20 01:58:40 +0300

    Updates

---
+++ precache-manifest.122718d930b591b073a8e20aefd7e5f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68744257b6b6bda5c9686c6f12fb28df",
+    "url": "/image-scroll-demo/index.html"
+  },
+  {
+    "revision": "6eb7d8a00ac55831702a",
+    "url": "/image-scroll-demo/static/js/2.4cc85b19.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/image-scroll-demo/static/js/2.4cc85b19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d84d499a33899958041",
+    "url": "/image-scroll-demo/static/js/main.2519e253.chunk.js"
+  },
+  {
+    "revision": "9cc9084a546167e95d1d",
+    "url": "/image-scroll-demo/static/js/runtime-main.bf6d3de7.js"
+  }
+]);
